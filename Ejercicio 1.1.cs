4523d60cbe8d36b7ce325e2f91459cdc495503f8
Joaco51976 <joaquincordini62@gmail.com>
2025-06-25 20:13:10 -0300

    Add files via upload

---
+++ Ejercicio 1.1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numerosvector = new int[100];
+            int contador = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Ingrese un numero entero, cuando desee dejar de ingresar numeros escriba -1");
+                int numero = Convert.ToInt32(Console.ReadLine());
+
+                if (numero == -1)
+                {
+                    break;
+                }
+                else
+                {
+                    numerosvector[contador] = numero;
+                    contador++;
+                }
+            }
+
+            Console.WriteLine("Listado de números ingresados:");
+            for (int i = 0; i < contador; i++)
+            {
+                Console.WriteLine(i + ": " + numerosvector[i]);
+            }
+
+            int suma = 0;
+            for (int i = 0; i < contador; i++)
+            {
+                suma += numerosvector[i];
+            }
+                double promedio = (double)suma / contador;
+
+                Console.WriteLine($"El promedio es: {promedio}");
+
+            int maximo = numerosvector[0];
+            int indiceMaximo = 0;
+
+            for (int i = 1; i < contador; i++)
+            {
+                if (numerosvector[i] > maximo)
+                {
+                    maximo = numerosvector[i];
+                    indiceMaximo = i;
+                }
+            }
+
+            Console.WriteLine("\nEl valor máximo es: " + maximo + " y se encuentra en la posición: " + indiceMaximo);
+
+        }
+    }
+}
